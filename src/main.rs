7fdab38f5d78e950719ffb40ac54bf075ecebc14
Nicola Da Rold <denetor@gmail.com>
2024-08-06 17:54:58 +0200

    add: echo typed chars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        // exit when encounters a 'q' key
+        if c == 'q' {
+            break;
+        }
+    }
 }
